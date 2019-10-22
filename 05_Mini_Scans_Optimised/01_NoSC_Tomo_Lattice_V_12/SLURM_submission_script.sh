b82d729416355ad54e749d0f5c3dfd106848128a
Haroon Rafique <haroon.rafique@cern.ch>
2019-10-22 18:59:45 +0200

    05: SLURM submission scripts updated on HPC-Batch

--- 05_Mini_Scans_Optimised/01_NoSC_Tomo_Lattice_V_12/SLURM_submission_script.sh
+++ 05_Mini_Scans_Optimised/01_NoSC_Tomo_Lattice_V_12/SLURM_submission_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=05_03_16
+#SBATCH --job-name=05_01_12
 #SBATCH --output=slurm.%N.%j.out
 #SBATCH --error=slurm.%N.%j.err
 #SBATCH --nodes=2
@@ -11,7 +11,7 @@
 #SBATCH --hint=nomultithread
 
 BATCH_ROOT_DIR=/hpcscratch/user/harafiqu
-RUN_DIR=/hpcscratch/user/harafiqu/PyORBIT_MD4224/05_Mini_Scans_Optimised/03_SbS_Tomo_Lattice_V_16
+RUN_DIR=/hpcscratch/user/harafiqu/PyORBIT_MD4224/05_Mini_Scans_Optimised/01_NoSC_Tomo_Lattice_V_12
 OrigIwd=$(pwd)
 
 # Make an output folder in the root directory to hold SLURM info file
