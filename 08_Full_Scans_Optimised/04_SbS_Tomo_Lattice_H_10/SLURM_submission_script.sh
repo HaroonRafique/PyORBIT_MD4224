fb25ed1f7bf045e54a1753fe57bf02c759d12ee3
Haroon Rafique <haroon.rafique@cern.ch>
2019-10-25 15:48:43 +0200

    08: 04: slurm scripts udpated

--- 08_Full_Scans_Optimised/04_SbS_Tomo_Lattice_H_10/SLURM_submission_script.sh
+++ 08_Full_Scans_Optimised/04_SbS_Tomo_Lattice_H_10/SLURM_submission_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=05_04_10
+#SBATCH --job-name=08_04_10
 #SBATCH --output=slurm.%N.%j.out
 #SBATCH --error=slurm.%N.%j.err
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=20
-#SBATCH --partition=inf-short
-#SBATCH --time=120:00:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=batch-long
+#SBATCH --time=504:00:00
 #SBATCH --mem-per-cpu=3200M
 #SBATCH --exclusive
 #SBATCH --hint=nomultithread
 
 BATCH_ROOT_DIR=/hpcscratch/user/harafiqu
-RUN_DIR=/hpcscratch/user/harafiqu/PyORBIT_MD4224/05_Mini_Scans_Optimised/04_SbS_Tomo_Lattice_H_10
+RUN_DIR=/hpcscratch/user/harafiqu/PyORBIT_MD4224/08_Full_Scans_Optimised/04_SbS_Tomo_Lattice_H_10
 OrigIwd=$(pwd)
 
 # Make an output folder in the root directory to hold SLURM info file
