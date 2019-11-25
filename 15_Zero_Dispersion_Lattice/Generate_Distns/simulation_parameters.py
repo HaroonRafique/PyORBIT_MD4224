432dc4a7ffe454fa459b9fd1405b5e35648396d3
Haroon Rafique <haroon.rafique@protonmail.com>
2019-11-25 13:19:29 +0100

    15: updated to generate distns on LXPlus

---
+++ 15_Zero_Dispersion_Lattice/Generate_Distns/simulation_parameters.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+parameters = {}
+
+parameters['n_macroparticles']		= int(50E3) #int(50E3) # int(0.5E6)
+parameters['lattice_version']	='Optimised'
+
+# Include machine (PS), tunes, lattice start position (BWS65H) for bunch output file label
+parameters['tunex']					= '621'
+parameters['tuney']					= '626'
+parameters['machine']				= 'PS'
+parameters['lattice_start'] 		= 'BSG48'
+parameters['bunch_label'] 		= parameters['machine'] + '_' + parameters['lattice_version'] + '_Lattice_Tune_' + parameters['tunex'] + '_' + parameters['tuney'] + '_' + parameters['lattice_start']
+parameters['flat_file']			= '../Optimised_Lattice/Flat_Files/config1_21_26/PTC-PyORBIT_flat_file.flt'
+#parameters['flat_file']			= '../Optimised_Lattice/Flat_Files/config2_21_25/PTC-PyORBIT_flat_file.flt'
+parameters['tomo_file']			= 'PyORBIT_Tomo_file_MD4224_HB.mat'
+
+# PTC RF Table Parameters
+harmonic_factors = [1] # this times the base harmonic defines the RF harmonics (for SPS = 4620, PS 10MHz 7, 8, or 9)
+time = np.array([0,1,2])
+ones = np.ones_like(time)
+Ekin_GeV = 1.4*ones
+RF_voltage_MV = np.array([0.0212942055190595723*ones]).T # in MV
+RF_phase = np.array([np.pi*ones]).T
+
+RFparameters = {
+	'harmonic_factors': harmonic_factors,
+	'time': time,
+	'Ekin_GeV': Ekin_GeV,
+	'voltage_MV': RF_voltage_MV,
+	'phase': RF_phase
+}
