a598d67e4c7d68ffde3eadbef4ddf41306a8452e
Haroon Rafique <haroon.rafique@protonmail.com>
2019-11-30 20:35:36 +0100

    17: 00 plots moved to Twiss_Comparisons

--- 17_PFW_Mismatch/00_Master/Make_SLURM_submission_script.py
+++ 17_PFW_Mismatch/00_Master/Make_SLURM_submission_script.py
@@ -19,11 +19,11 @@ clean_all = True		# Clean simulation folder before running (False when resuming 
 # Must be chosen
 
 # ~ queue = 'inf-long', 'inf-short', 'batch-long', 'batch-short'
-queue = 'inf-long'
+queue = 'batch-long'
 
 n_nodes = 4 
 
-jobname = '14_04_23'
+jobname = '17_01_25'
 
 path_to_simulation = os.path.dirname(os.path.realpath(__file__)) # This directory
 
