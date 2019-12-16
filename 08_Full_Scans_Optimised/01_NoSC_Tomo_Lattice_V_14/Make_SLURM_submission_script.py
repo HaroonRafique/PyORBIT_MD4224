b3f29f2d603c282647fb20b6ab4e755da6a01c78
Haroon Rafique <haroon.rafique@protonmail.com>
2019-12-16 15:24:56 +0100

    08: NoSC V scans udpated for submission on HPC-Batch

--- 08_Full_Scans_Optimised/01_NoSC_Tomo_Lattice_V_14/Make_SLURM_submission_script.py
+++ 08_Full_Scans_Optimised/01_NoSC_Tomo_Lattice_V_14/Make_SLURM_submission_script.py
@@ -21,7 +21,7 @@ clean_all = True		# Clean simulation folder before running (False when resuming 
 # ~ queue = 'inf-long', 'inf-short', 'batch-long', 'batch-short'
 queue = 'batch-long'
 
-n_nodes = 4 
+n_nodes = 2 
 
 jobname = '08_01_14'
 
