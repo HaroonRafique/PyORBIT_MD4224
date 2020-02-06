ca04cc531c4e0ac198f8e0bab8fafd1533eaa343
Haroon Rafique <haroon.rafique@protonmail.com>
2020-02-06 15:30:17 +0100

    06: 00: updated for sims on LXPlus

--- 06_Injection_Bump/00_Master/PyORBIT/pyOrbit.py
+++ 06_Injection_Bump/00_Master/PyORBIT/pyOrbit.py
@@ -407,7 +407,7 @@ for turn in range(sts['turn']+1, sts['turns_max']):
 	bunchtwissanalysis.analyzeBunch(bunch)  # analyze twiss and emittance
 	moments = BunchGather(bunch, turn, p)	# Calculate bunch moments and kurtosis
 	if s['Update_Twiss']: 
-		readScriptPTC_noSTDOUT("PTC/update-twiss.ptc") # this is needed to correclty update the twiss functions in all lattice nodes in updateParamsPTC
+		readScriptPTC_noSTDOUT("../PTC/update-twiss.ptc") # this is needed to correclty update the twiss functions in all lattice nodes in updateParamsPTC
 		updateParamsPTC(Lattice,bunch) 			# to update bunch energy and twiss functions
 
 	if turn in sts['turns_update']:	sts['turn'] = turn
