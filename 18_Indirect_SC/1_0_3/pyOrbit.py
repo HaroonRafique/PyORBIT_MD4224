ab730b8a2d3d0368f19517e40bc93bacaa8d9f36
Haroon Rafique <haroon.rafique@protonmail.com>
2020-06-24 13:31:20 +0200

    18/19: updated

--- 18_Indirect_SC/1_0_3/pyOrbit.py
+++ 18_Indirect_SC/1_0_3/pyOrbit.py
@@ -203,7 +203,7 @@ if sts['turn'] < 0:
 #-----------------------------------------------------------------------
 		print '\ngenerate_initial_distribution on MPI process: ', rank
 		# ~ Particle_distribution_file = generate_initial_distribution_from_tomo_manual_Twiss(p, twiss_dict, 1, output_file='input/ParticleDistribution.in', summary_file='input/ParticleDistribution_summary.txt')
-		Particle_distribution_file = generate_initial_distribution_from_tomo(p, 1, output_file='input/ParticleDistribution.in', summary_file='input/ParticleDistribution_summary.txt')
+		Particle_distribution_file = generate_initial_distribution_from_tomo(p,  1, Lattice, output_file='input/ParticleDistribution.in', summary_file='input/ParticleDistribution_summary.txt')
 
 		print '\bunch_orbit_to_pyorbit on MPI process: ', rank
 		bunch_orbit_to_pyorbit(paramsDict["length"], kin_Energy, Particle_distribution_file, bunch, p['n_macroparticles'] + 1) #read in only first N_mp particles.
